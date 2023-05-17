fe74fff1dd2a0e122568d24b5c8bf689a2a7476a
Gordon Smith <GordonJSmith@gmail.com>
2023-05-17 08:51:31 +0100

    WIP

--- guest/cpp/wasi-sdk/add/main.cpp
+++ guest/cpp/wasi-sdk/add/main.cpp
@@ -2,10 +2,16 @@
 
 uint32_t test_add(uint32_t a, uint32_t b)
 {
+    test_string_t msg;
+    test_string_set(&msg, "test_add");
+    test_print(&msg);
     return a + b;
 }
 
 uint32_t test_sub(uint32_t a, uint32_t b)
 {
+    test_string_t msg;
+    test_string_set(&msg, "test_sub");
+    test_print(&msg);
     return a - b;
 }
--- host/cpp/wamr/main/main.cpp
+++ host/cpp/wamr/main/main.cpp
@@ -44,24 +44,32 @@ struct WASMRuntime
     }
 };
 
+void print(wasm_exec_env_t exec_env, char *msg, uint32_t msg_len)
+{
+    std::string s = msg;
+    printf("print: %s\n", s.c_str());
+}
+
+static NativeSymbol native_symbols[] = {
+    {"print", (void *)print, "(*~)"},
+};
+
 int main(int argc, const char *argv[])
 {
     char error_buf[128];
-    wasm_function_inst_t func;
     uint32_t stack_size = 8092, heap_size = 8092;
 
     WASMRuntime runtime;
 
+    int n_native_symbols = sizeof(native_symbols) / sizeof(NativeSymbol);
+    wasm_runtime_register_natives("$root", native_symbols, n_native_symbols);
+
     auto buffer = read_wasm_binary_to_buffer(wasmFile2);
-
-    /* add line below if we want to export native functions to WASM app */
-    // wasm_runtime_register_natives(...);
-
     std::unique_ptr<WASMModuleCommon, decltype(&wasm_runtime_unload)> module(wasm_runtime_load(buffer.data(), buffer.size(), error_buf, sizeof(error_buf)), &wasm_runtime_unload);
-
     std::unique_ptr<WASMModuleInstanceCommon, decltype(&wasm_runtime_deinstantiate)> module_inst(wasm_runtime_instantiate(module.get(), stack_size, heap_size, error_buf, sizeof(error_buf)), &wasm_runtime_deinstantiate);
 
-    func = wasm_runtime_lookup_function(module_inst.get(), "add", NULL);
+    wasm_function_inst_t addFunc = wasm_runtime_lookup_function(module_inst.get(), "add", NULL);
+    wasm_function_inst_t subFunc = wasm_runtime_lookup_function(module_inst.get(), "sub", NULL);
 
     /* creat an execution environment to execute the WASM functions */
     std::unique_ptr<WASMExecEnv, decltype(&wasm_runtime_destroy_exec_env)> exec_env(wasm_runtime_create_exec_env(module_inst.get(), stack_size), &wasm_runtime_destroy_exec_env);
@@ -70,8 +78,13 @@ int main(int argc, const char *argv[])
     uint32_t addArgv[2];
     addArgv[0] = 11;
     addArgv[1] = 22;
-    wasm_runtime_call_wasm(exec_env.get(), func, 2, addArgv);
+    wasm_runtime_call_wasm(exec_env.get(), addFunc, 2, addArgv);
     printf("add function return: %i\n", addArgv[0]);
+    uint32_t subArgv[2];
+    subArgv[0] = 100;
+    subArgv[1] = 1;
+    wasm_runtime_call_wasm(exec_env.get(), subFunc, 2, subArgv);
+    printf("sub function return: %i\n", subArgv[0]);
 
     return 0;
 }
